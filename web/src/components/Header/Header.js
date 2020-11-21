c57fdedb25246f46348e6419a66582dc69fb2971
Andrey Lapshov <a.lapshov@dataduck.com>
2020-11-21 15:15:03 +0300

    styles fixes

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -1,47 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Header.scss';
 import { ReactComponent as YaIcon } from 'icons/ya-light.svg';
 import { ReactComponent as PrIcon } from 'icons/pr-dark.svg';
 import { ReactComponent as EllipseIcon } from 'icons/ellipse.svg';
 import { ReactComponent as TFIcon } from 'icons/tf-logo.svg';
 import MenuItem from '../MenuItem/MenuItem';
-
-const links = [
-  { value: 'О проекте', path: '/project' },
-  { value: 'Данные', path: '/main' },
-  { value: 'О нас', path: '/about' },
-];
+import links from '../../constants/links';
 
 const Header = () => {
-  const [activeMenuItem, setActiveMenuItem] = useState('О проекте');
-  const handleActiveMenuItem = id => {
-    setActiveMenuItem(id);
-  };
-
   return (
     <header className="header">
       <div className="header__logo">
         <a href="https://yandex.ru/">
-          <YaIcon className="header__logo-ya" />
+          <YaIcon className="logo-icon header__logo-ya" />
         </a>
         <a href="https://praktikum.yandex.ru/">
-          <PrIcon />
+          <PrIcon className="logo-icon" />
         </a>
         <EllipseIcon className="header__logo-ellipse" />
         <a href="https://teddyfood.com/ru/">
-          <TFIcon />
+          <TFIcon className="logo-icon" />
         </a>
       </div>
       <nav>
         <ul className="header__menu">
           {links.map(({ value, path }) => (
-            <MenuItem
-              key={value}
-              text={value}
-              clickHandler={handleActiveMenuItem}
-              isActive={value === activeMenuItem}
-              path={path}
-            />
+            <MenuItem key={value} text={value} path={path} />
           ))}
         </ul>
       </nav>
